a487f97b5ebf04cc4cef2ce2d3ccfe764ef169c6
htldacosta <hitaludcosta@gmail.com>
2026-08-03 16:21:16 -0400

    class enum was created and completed

---
+++ src/enums/Caracteristicas.java
@@ -0,0 +1,18 @@
+package enums;
+
+public enum Caracteristicas {
+    MAMIFERO,
+    AQUATICO,
+    AVE,
+    REPTIL,
+    CARNIVORO,
+    HERBIVORO,
+    BOTA_OVO,
+    VENENOSO,
+    ANIMAL_DOMESTICO,
+    TEM_LISTRAS,
+    MUITO_PESADO,
+    VOADOR,
+    PULA_ALTO,
+    TEM_GARRAS
+}
